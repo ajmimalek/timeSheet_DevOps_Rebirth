68198af78a7a995b5f03c96248de2981cb4b3150
Malek Ajmi <45561089+ajmimalek@users.noreply.github.com>
2021-10-16 16:11:28 +0100

    Initial commit

---
+++ src/main/java/tn/esprit/spring/config/LoggingAspect.java
@@ -0,0 +1,26 @@
+package tn.esprit.spring.config;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class LoggingAspect {
+	private static final Logger l = LogManager.getLogger(LoggingAspect.class);
+	@Around("execution(* tn.esprit.spring.service.*.*(..))")
+	public Object profile(ProceedingJoinPoint pjp) throws Throwable {
+	long start = System.currentTimeMillis();
+	Object obj = pjp.proceed();
+	long elapsedTime = System.currentTimeMillis() - start;
+	if (elapsedTime > 3000) {
+		l.fatal("This process takes more than 3sec to execute");
+	}
+	l.info("Method execution time: " + elapsedTime + " milliseconds.");
+	return obj;
+	}
+
+}
