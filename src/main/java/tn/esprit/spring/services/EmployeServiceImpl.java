81f5229f687694c2423be3638a493f12e2e6d723
aguel achref <57445001+achref-hub@users.noreply.github.com>
2021-11-10 14:48:05 +0100

    fixed errors

--- src/main/java/tn/esprit/spring/services/EmployeServiceImpl.java
+++ src/main/java/tn/esprit/spring/services/EmployeServiceImpl.java
@@ -96,9 +96,7 @@ public class EmployeServiceImpl implements IEmployeService {
 	{
 		Employe employe = employeRepository.findById(employeId).get();
 
-		//Desaffecter l'employe de tous les departements
-		//c'est le bout master qui permet de mettre a jour
-		//la table d'association
+		
 		for(Departement dep : employe.getDepartements()){
 			dep.getEmployes().remove(employe);
 		}
