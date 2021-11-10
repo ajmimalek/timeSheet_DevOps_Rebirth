c99000d6dbf569397304538f973b84629cfa859f
aguel achref <57445001+achref-hub@users.noreply.github.com>
2021-11-10 13:41:53 +0100

    update TestUnitaire

---
+++ src/test/java/tn/esprit/spring/services/DepartementServiceImpTest.java
@@ -0,0 +1,31 @@
+package tn.esprit.spring.services;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import tn.esprit.spring.entities.Departement;
+import tn.esprit.spring.repository.DepartementRepository;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+
+
+public class DepartementServiceImpTest {
+	
+	@Autowired
+	IEntrepriseService es;
+	DepartementRepository depp;
+	
+	@Test
+	public void testAjouterDepartement(){
+		Departement dep = new Departement("RH");
+		int res = es.ajouterDepartement(dep);			
+		assertEquals(dep.getId(), res);
+		
+	}
+
+}
